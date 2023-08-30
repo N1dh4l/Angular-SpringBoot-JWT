e61f99f146e5f93113f0d4c0152514b997fd58c0
N1dhAl <nidtar123@gmail.com>
2023-08-30 01:57:48 +0100

    first commit: Login UI

---
+++ JwtAuthentification_Angular16_SpringBoot3/back-end/jwtAuthentification/src/main/java/com/projects/jwtAuthentification/JwtAuthentificationApplication.java
@@ -0,0 +1,13 @@
+package com.projects.jwtAuthentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthentificationApplication.class, args);
+	}
+
+}
